07175c935c1e02acffa0ffd55c243dacb97b2c7b
Michał Wójcik <michalwojcik@macbook-pro-micha.home>
2021-01-17 02:11:13 +0100

    index CSS href change and searchbar parenthases missing fixed

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -9,8 +9,17 @@ const sortByOptions = {
 };
 
 class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = '';
+        this.setState({
+            term: '',
+            location: '',
+            sortBy: 'best_match'
+        });
+    }
     renderSortByOptions(){
-        return Object.keys(sortByOptions).map( sortByOption => {
+        return Object.keys(sortByOptions).map(sortByOption => {
             let sortByOptionValue = sortByOptions[sortByOption];
             return <li key={sortByOptionValue}>{sortByOption}</li>;
         });
@@ -20,7 +29,7 @@ class SearchBar extends React.Component {
             <div className="SearchBar">
                 <div className="SearchBar-sort-options">
                     <ul>
-                        {this.renderSortByOptions};
+                        {this.renderSortByOptions()};
                     </ul>
                 </div>
                 <div className="SearchBar-fields">
